12e976b4694240dcced802077a536f44a41bc64a
Kirill Morozov (moki) <morozov.kirill.moki@gmail.com>
2021-12-09 19:44:49 +0300

    feat(2021/typescript): add day-09 part-2 solution

--- 2021/typescript/src/day-09/solution.ts
+++ 2021/typescript/src/day-09/solution.ts
@@ -37,8 +37,62 @@ function part_1(input: string) {
     return sum;
 }
 
+function walk(
+    data: number[][],
+    i: number,
+    j: number,
+    visited: boolean[][]
+): number {
+    if (
+        i < 0 ||
+        j < 0 ||
+        i >= visited.length ||
+        j >= visited[0].length ||
+        visited[i][j]
+    )
+        return 0;
+
+    visited[i][j] = true;
+
+    return (
+        1 +
+        walk(data, i - 1, j, visited) +
+        walk(data, i, j + 1, visited) +
+        walk(data, i + 1, j, visited) +
+        walk(data, i, j - 1, visited)
+    );
+}
+
 function part_2(input: string) {
-    return 0;
+    const data = parse_input(input);
+
+    const ps = [];
+
+    for (let i = 0; i < data.length; i++) {
+        for (let j = 0; j < data[i].length; j++) {
+            const adj = get_adjacent(data, i, j);
+
+            if (less_than(adj, data[i][j])) {
+                ps.push([i, j]);
+            }
+        }
+    }
+
+    const visited = new Array(data.length)
+        .fill([])
+        .map((a, i) =>
+            new Array(data[0].length)
+                .fill(false)
+                .map((v, j) => (data[i][j] === 9 ? true : false))
+        );
+
+    const bs = [];
+
+    for (let [i, j] of ps) bs.push(walk(data, i, j, visited));
+
+    bs.sort((a, b) => b - a);
+
+    return bs[0] * bs[1] * bs[2];
 }
 
 export { part_1, part_2 };
